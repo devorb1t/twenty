37320778b3527b71f8771dd94b695c932e2a3e2c
devorb1t <itsjoshua.cho@gmail.com>
2026-03-31 12:23:20 +0200

    add skeleton when navigation drawer state is loading

--- packages/twenty-front/src/modules/navigation/components/MainNavigationDrawerScrollableItems.tsx
+++ packages/twenty-front/src/modules/navigation/components/MainNavigationDrawerScrollableItems.tsx
@@ -1,8 +1,10 @@
+import { metadataStoreState } from '@/metadata-store/states/metadataStoreState';
 import { NavigationDrawerOpenedSection } from '@/navigation-menu-item/display/sections/components/NavigationDrawerOpenedSection';
 import { FavoritesSectionDispatcher } from '@/navigation-menu-item/display/sections/favorites/components/FavoritesSectionDispatcher';
 import { WorkspaceSectionDispatcher } from '@/navigation-menu-item/display/sections/workspace/components/WorkspaceSectionDispatcher';
-
 import { NavigationDrawerOtherSection } from '@/navigation/components/NavigationDrawerOtherSection';
+import { NavigationDrawerWorkspaceSectionSkeletonLoader } from '@/object-metadata/components/NavigationDrawerWorkspaceSectionSkeletonLoader';
+import { useAtomFamilyStateValue } from '@/ui/utilities/state/jotai/hooks/useAtomFamilyStateValue';
 import { styled } from '@linaria/react';
 
 import { themeCssVariables } from 'twenty-ui/theme-constants';
@@ -14,11 +16,24 @@ const StyledScrollableItemsContainer = styled.div`
 `;
 
 export const MainNavigationDrawerScrollableItems = () => {
+  const metadataStore = useAtomFamilyStateValue(
+    metadataStoreState,
+    'navigationMenuItems',
+  );
+
+  const isNavigationMenuItemsLoading = metadataStore.status === 'empty';
+
   return (
     <StyledScrollableItemsContainer>
       <NavigationDrawerOpenedSection />
-      <FavoritesSectionDispatcher />
-      <WorkspaceSectionDispatcher />
+      {isNavigationMenuItemsLoading ? (
+        <NavigationDrawerWorkspaceSectionSkeletonLoader />
+      ) : (
+        <>
+          <FavoritesSectionDispatcher />
+          <WorkspaceSectionDispatcher />
+        </>
+      )}
       <NavigationDrawerOtherSection />
     </StyledScrollableItemsContainer>
   );
