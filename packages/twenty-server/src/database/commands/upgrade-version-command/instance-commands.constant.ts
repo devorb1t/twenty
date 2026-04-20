21e68cbdf091472090e07b757d804f9b7384eb89
devorb1t <itsjoshua.cho@gmail.com>
2026-04-20 18:07:04 +0200

    Hotfix v1.22.6

--- packages/twenty-server/src/database/commands/upgrade-version-command/instance-commands.constant.ts
+++ packages/twenty-server/src/database/commands/upgrade-version-command/instance-commands.constant.ts
@@ -3,6 +3,7 @@
 import { AddViewFieldGroupIdIndexOnViewFieldFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-21/1-21-instance-command-fast-1775129420309-add-view-field-group-id-index-on-view-field';
 import { MigrateMessagingCalendarToCoreFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-21/1-21-instance-command-fast-1775165049548-migrate-messaging-calendar-to-core';
 import { AddEmailThreadWidgetTypeFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-21/1-21-instance-command-fast-1775200000000-add-email-thread-widget-type';
+import { AddConditionalAvailabilityExpressionToPageLayoutWidgetFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-22/1-22-instance-command-fast-1775749486400-add-conditional-availability-expression-to-page-layout-widget';
 import { AddPermissionFlagRoleIdIndexFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-22/1-22-instance-command-fast-1775749486425-add-permission-flag-role-id-index';
 import { AddWorkspaceIdToIndirectEntitiesFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-22/1-22-instance-command-fast-1775758621017-add-workspace-id-to-indirect-entities';
 import { AddWorkspaceIdIndexesAndFksFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-22/1-22-instance-command-fast-1775761294897-add-workspace-id-indexes-and-fks-to-indirect-entities';
@@ -12,6 +13,7 @@ import { BackfillWorkspaceIdOnIndirectEntitiesSlowInstanceCommand } from 'src/da
 import { DropWorkspaceVersionColumnFastInstanceCommand } from 'src/database/commands/upgrade-version-command/1-23/1-23-instance-command-fast-1785000000000-drop-workspace-version-column';
 
 export const INSTANCE_COMMANDS = [
+  AddConditionalAvailabilityExpressionToPageLayoutWidgetFastInstanceCommand,
   AddViewFieldGroupIdIndexOnViewFieldFastInstanceCommand,
   MigrateMessagingCalendarToCoreFastInstanceCommand,
   AddEmailThreadWidgetTypeFastInstanceCommand,
